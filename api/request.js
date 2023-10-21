eedb3bdc3f7375c98799dcb1b3e7a56d9918ad84
fardinkamal62 <fardinkamal62@protonmail.ch>
2023-10-21 19:07:34 +0600

    session-4

--- api/request.js
+++ api/request.js
@@ -31,30 +31,40 @@ create.createRequest = async (req) => {
     return await database.client.collection(collections.REQUESTS).insertOne(payload);
 }
 
-create.getRequest = async (req) => {
+create.getRequests = async (req) => {
     const { userId } = req.user;
-    const { item, itemNeeded, drop, dropLocation, time, fare } = req.body;
-    
-    const payload = {
-        uid: userId,
+
+    const limit = parseInt(req.query.limit) || 5;
+    const page = parseInt(req.query.page) || 1;
+    const uid = req.query.uid || userId;
+    const startTime = req.query.startTime || 0;
+    const endTime = req.query.endTime || 0;
+
+    const key ={
+        uid,
+    }
+
+    if(startTime && endTime) {
+        key.time = {
+            $gte: startTime,
+            $lte: endTime,
+        }
+    }
+
+    const count = await database.client.collection(collections.REQUESTS).countDocuments(key);
+    const totalPages = Math.ceil(count / limit);
+    const offset = (page - 1) * limit;
+
+    const requests = await database.client.collection(collections.REQUESTS)
+        .find(key)
+        .skip(offset)
+        .limit(limit)
+        .toArray();
+
+    return {
+        requests,
+        totalPages,
+        currentPage: page,
+        totalDocuments: count,
     };
-
-    if (!item && !drop) throw new Error('Item or drop is required');
-    if (!time || !fare) throw new Error('Time & fair is required');
-    payload.time = time;
-    payload.fare = fare;
-
-    if (item === 'true') {
-        if(!itemNeeded) throw new Error('Item needed is required');
-        payload.item = true;
-        payload.itemNeeded = itemNeeded;
-    }
-
-    if (drop === 'true') {
-        if(!dropLocation) throw new Error('Drop location is required');
-        payload.drop = true;
-        payload.dropLocation = dropLocation;
-    }
-
-    return await database.client.collection(collections.REQUESTS).insertOne(payload);
 }
--- controllers/api/request.js
+++ controllers/api/request.js
@@ -6,3 +6,7 @@ const requestController = module.exports;
 requestController.createRequest = async (req, res) => {
 	helpers.formatApiResponse(200, res, await apiHandlers.request.createRequest(req));
 };
+
+requestController.getRequests = async (req, res) => {
+	helpers.formatApiResponse(200, res, await apiHandlers.request.getRequests(req));
+};
