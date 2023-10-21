adfe9084ad2da2097a8bc98c8f9ace35466841c6
fardinkamal62 <fardinkamal62@protonmail.ch>
2023-10-21 21:12:23 +0600

    create request API

--- api/request.js
+++ api/request.js
@@ -1,14 +1,16 @@
 const database = require("../database");
 const { collections } = require("../database");
 
-const create = module.exports;
+const request = module.exports;
 
-create.createRequest = async (req) => {
+request.createRequest = async (req) => {
     const { userId } = req.user;
     const { item, itemNeeded, drop, dropLocation, time, fare } = req.body;
-    
+
     const payload = {
         uid: userId,
+        createdAt: new Date().toISOString(),
+        status: 'pending',
     };
 
     if (!item && !drop) throw new Error('Item or drop is required');
@@ -17,44 +19,16 @@ create.createRequest = async (req) => {
     payload.fare = fare;
 
     if (item === 'true') {
-        if(!itemNeeded) throw new Error('Item needed is required');
+        if (!itemNeeded) throw new Error('Item needed is required');
         payload.item = true;
         payload.itemNeeded = itemNeeded;
     }
 
     if (drop === 'true') {
-        if(!dropLocation) throw new Error('Drop location is required');
+        if (!dropLocation) throw new Error('Drop location is required');
         payload.drop = true;
         payload.dropLocation = dropLocation;
     }
 
     return await database.client.collection(collections.REQUESTS).insertOne(payload);
-}
-
-create.getRequest = async (req) => {
-    const { userId } = req.user;
-    const { item, itemNeeded, drop, dropLocation, time, fare } = req.body;
-    
-    const payload = {
-        uid: userId,
-    };
-
-    if (!item && !drop) throw new Error('Item or drop is required');
-    if (!time || !fare) throw new Error('Time & fair is required');
-    payload.time = time;
-    payload.fare = fare;
-
-    if (item === 'true') {
-        if(!itemNeeded) throw new Error('Item needed is required');
-        payload.item = true;
-        payload.itemNeeded = itemNeeded;
-    }
-
-    if (drop === 'true') {
-        if(!dropLocation) throw new Error('Drop location is required');
-        payload.drop = true;
-        payload.dropLocation = dropLocation;
-    }
-
-    return await database.client.collection(collections.REQUESTS).insertOne(payload);
-}
+};
