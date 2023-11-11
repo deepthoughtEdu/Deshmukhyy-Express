d034a324b5809c89d372f7f5f004e3593a38d49c
Shawan Mandal <shawan.sm049@gmail.com>
2023-11-11 19:28:34 +0530

    Integrated create note screen with API

---
+++ public/javascript/notes-nest/index.js
@@ -0,0 +1,39 @@
+$(window).on('load', initialize);
+
+function initialize() {
+
+    $('#note-form').on('submit', function (e) {
+        e.preventDefault();
+
+        const formData = {};
+        const data = $(this).serializeArray();
+
+        data.forEach(({ name, value }) => {
+            formData[name] = value;
+        })
+
+        $.ajax({
+            url: '/api/note',
+            method: 'post',
+            cache: false,
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            data: JSON.stringify(formData),
+            success: function () {
+                Swal.fire(
+                    'Success!',
+                    'Note created successfully.',
+                    'success'
+                )
+            },
+            error: function () {
+                Swal.fire(
+                    'Error!',
+                    'Something went wrong.',
+                    'error'
+                );
+            }
+        })
+    });
+
+}
