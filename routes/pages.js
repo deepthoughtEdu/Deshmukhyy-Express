0c394adaef7419820ab67c12b0f0896885932837
Shawan Mandal <shawan.sm049@gmail.com>
2023-11-14 13:11:23 +0530

    Revert "merged"
    This reverts commit 8365acacacbb63b018d77fbde8ce97c8a1570694.

--- routes/pages.js
+++ routes/pages.js
@@ -9,12 +9,16 @@ helpers.setupPageRoute(router, '/', [middlewares.user.requireLogin], controllers
 helpers.setupPageRoute(router, '/login', [], controllers.pages.login)
 
 helpers.setupPageRoute(router, '/deshmukhi-express', [], controllers.pages.deshmukhiExpress.home);
+<<<<<<< HEAD
 helpers.setupPageRoute(router, '/deshmukhi-express/dashboard', [], controllers.pages.deshmukhiExpress.dashboard);
 helpers.setupPageRoute(router, '/deshmukhi-express/create', [], controllers.pages.deshmukhiExpress.create);
 
 helpers.setupPageRoute(router, '/movies-sync', [], controllers.pages.movieSync.home);
 helpers.setupPageRoute(router, '/movies-sync/create', [], controllers.pages.movieSync.create);
 
+=======
+helpers.setupPageRoute(router, '/movies-sync', [], controllers.pages.movieSync.home);
+>>>>>>> dataDrivenProblemDiagnosis
 helpers.setupPageRoute(router, '/notes-nest', [], controllers.pages.notesNest.home);
 helpers.setupPageRoute(router, '/notes-nest/create', [], controllers.pages.notesNest.create);
 
