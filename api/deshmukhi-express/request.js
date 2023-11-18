eced42b1d8b79bff1ce82ada4534b462df4095cb
Shagun Mishra <shagunmishra.vns@gmail.com>
2023-11-19 00:31:39 +0530

    added update API

--- api/deshmukhi-express/request.js
+++ api/deshmukhi-express/request.js
@@ -4,6 +4,7 @@ const { ObjectId } = require("mongodb");
 const utilities = require("../../utilities");
 
 const request = module.exports;
+const validStatus = ['approved', 'waiting', 'cancelled'];
 
 request.create = async (req) => {
     const { userId } = req.user;
@@ -42,4 +43,22 @@ request.get = async (req) => {
     }));
 
     return utilities.paginate(`/api/request${req.url}`, requestsWithUserData, count, limit, page);
+};
+
+request.update = async (req) => {
+    const { userId } = req.user;
+    const { status } = req.body;
+    const id = req.params.id;
+
+    const searchKeys = { uid:userId };
+    const payload = {}
+    if (!validStatus.includes(status)) throw new Error("Invalid status supplied!");
+    if (!ObjectId.isValid(id)) throw new Error("Invalid request ID!")
+    
+    searchKeys._id = new ObjectId(id);
+    console.log(searchKeys)
+    payload.status = status;
+    payload.updatedAt = utilities.getISOTimestamp();
+
+    await database.client.collection(collections.REQUESTS).findOneAndUpdate(searchKeys, { $set: payload });
 };
