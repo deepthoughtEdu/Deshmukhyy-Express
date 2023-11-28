4e373618a3d0993f2589856a1a649b78d1958843
Shagun Mishra <shagunmishra.vns@gmail.com>
2023-11-28 10:07:52 +0530

    changed the status of the delivery dashboard

--- business-logic/app/update.js
+++ business-logic/app/update.js
@@ -4,7 +4,7 @@ const { ObjectId } = require("mongodb");
 const utilities = require("../../utilities")
 
 const update = module.exports;
-const validStatus = ['approved', 'pending', 'cancelled'];
+const validStatus = ['approved', 'waiting', 'cancelled'];
 
 update.logic = async (req) => {
     const { userId } = req.user;
--- public/javascript/app/delivery/dashboard.js
+++ public/javascript/app/delivery/dashboard.js
@@ -1,5 +1,35 @@
 $(window).on('load', initialize);
 
+const classes = {
+    type:{
+        cancelled:"table-danger",
+        approved: "table-success",
+        waiting:  "table-warning"
+    }
+}
+
+let types = {
+    approved : {
+        label:"Approved",
+        isSelected:false
+    },
+    waiting : {
+        label:"Wait",
+        isSelected:false
+    },
+    cancelled : {
+        label:"Cancel",
+        isSelected:false
+    }
+};
+
+function getSelect(selected){
+  let html =  Object.keys(types).map(type => {
+        return ` <option ${type == selected ? 'selected="true"' : ""} value="${type}">${types[type].label}</option>`
+  }).join('');
+  return  `<select class="custom-select status" name="status">${html}</select>`
+}
+
 function initialize() {
     renderNewRequestsTable();
     renderAcceptedRequestsTable();
@@ -69,13 +99,51 @@ function renderNewRequestsTable () {
                     category: category.charAt(0).toUpperCase() + category.slice(1),
                     time: row.time,
                     fare: row.fare,
-                    action: `<button data-request-id="${row._id}" class="btn btn-info">Accept</button>`
+                    action: getSelect(row.status || 'approved')
                 }
             }
         })
     }
     
     orderDetailsTable.render(`/api/app?role=delivery-partner&status=pending`);
+
+    $('#order-details').on('change','select.status',function(){
+        let value = $(this).val();
+        let _classes = Object.keys(classes.type).map(e => classes.type[e]).join(' ');
+        let _class = classes.type[value] || "";
+
+        let $tr = $(this).parents('tr').first();
+        let id = $tr.data('id');
+       
+        $tr.removeClass(_classes).addClass(_class);
+
+        let data = {
+            status: value
+        }
+
+        $.ajax({
+            url: `/api/app/${id}`,
+            method: 'put',
+            cache: false,
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            data: JSON.stringify(data),
+            success: function () {
+                Swal.fire(
+                    'Success!',
+                    'Order status updated successfully.',
+                    'success'
+                )
+            },
+            error: function () {
+                Swal.fire(
+                    'Error!',
+                    'Something went wrong.',
+                    'error'
+                );
+            }
+        })
+    })
 }
 
 function renderAcceptedRequestsTable () {
@@ -155,3 +223,9 @@ function renderCompletedRequestsTable () {
     }
     orderDetailsTable.render(`/api/app?role=delivery-partner&status=completed&acceptedBy=${loggedInUser.userId}`);
 }
+
+
+
+
+
+
