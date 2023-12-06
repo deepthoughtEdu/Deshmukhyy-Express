44460f435b7878ec0467f3b2a4c32f2b405f1d1e
Shawan Mandal <shawan.sm049@gmail.com>
2023-12-06 12:34:39 +0530

    Update stylesheet

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -55,15 +55,9 @@ export default function Home (props) {
             style={{ bottom: "60px", right: "60px" }}>
 
             <Button
-              className="btn-dark"
+              className="btn-dark create-btn-rounded"
               onClick={handleShow}
-              style={{
-                borderRadius: "40px",
-                height: "60px",
-                width: "60px",
-                boxShadow:
-                  "rgba(0, 0, 0, 0.2) 0px 2px 4px -1px, rgba(0, 0, 0, 0.14) 0px 4px 5px 0px, rgba(0, 0, 0, 0.12) 0px 1px 10px 0px",
-              }}>
+              >
 
               <FontAwesomeIcon icon={faPlus} />
             </Button>
